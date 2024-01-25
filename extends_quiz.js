914d7d292f51fd167d9785e744fc2c4ab346ebda
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-01-25 13:19:01 +0900

    first commit

---
+++ extends_quiz.js
@@ -0,0 +1,37 @@
+class Food {
+	constructor(name) {
+		this.name = name;
+		this.brands = [];
+	}
+
+	addBrand(brand) {
+		this.brands.push(brand);
+	}
+
+	print() {
+		console.log(`${this.name}을/를 파는 음식점들 : `); 
+		console.log(this.brands.join(' , '));
+	}
+}
+
+class Pizza extends Food {
+	constructor() {
+		super('피자');
+	}
+}
+
+class Chicken extends Food {
+	constructor() {
+		super('치킨');
+	}
+}
+
+const pizza = new Pizza();
+pizza.addBrand('피자헛');
+pizza.addBrand('도미노피자');
+const chicken = new Chicken();
+chicken.addBrand('굽네치킨');
+chicken.addBrand('BBQ');
+
+pizza.print();
+chicken.print();
